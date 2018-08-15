9b3c7c70ccd2d8c4de1aa03e408f291a91ff3991
Artyom Keydunov <artyom.keydunov@gmail.com>
2018-08-15 19:20:46 +0300

    Stripe SaaS Metrics Cube

--- StripeSaaSMetrics.js
+++ StripeSaaSMetrics.js
@@ -1,139 +1,169 @@
-const paidInvoicesSubquery = `SELECT
-            *,
-            (
-              CASE
-              WHEN plan_interval = 'month' THEN
-                (invoice_amount - discount_amount)
-              WHEN plan_interval = 'year' THEN
-                (invoice_amount - discount_amount) / 12
-              END
-            ) / 100.0 AS mrr
-          FROM
-            (
-              SELECT
-                ${SCHEMA}.invoices.id as invoice_id,
-                ${SCHEMA}.invoices.date as created,
-                ${SCHEMA}.invoices.currency,
-                ${SCHEMA}.subscriptions.ended_at,
-                LAST_VALUE(${SCHEMA}.invoices.id) OVER(
-                  PARTITION BY ${SCHEMA}.invoices.subscription_id
-                  ORDER BY
-                    ${SCHEMA}.invoices.date ROWS BETWEEN UNBOUNDED PRECEDING
-                  AND UNBOUNDED FOLLOWING
-                ) AS moment_last_invoice_id,
-                ${SCHEMA}.invoices.customer_id,
-                ${SCHEMA}.invoices.subscription_id,
-                ${SCHEMA}.invoices_items.plan_id,
-                ${SCHEMA}.invoices_items.plan->>'interval' as plan_interval,
-                ${SCHEMA}.invoices_items.plan->>'name' as plan_name,
-                (${SCHEMA}.invoices.subtotal - (${SCHEMA}.invoices.total - coalesce(${SCHEMA}.invoices.tax, 0))) as discount_amount,
-                ${SCHEMA}.invoices_items.amount as invoice_amount
-              FROM
-                ${SCHEMA}.invoices_items
-                INNER JOIN ${SCHEMA}.invoices ON (${SCHEMA}.invoices.id = ${SCHEMA}.invoices_items.invoice_id)
-                LEFT JOIN ${SCHEMA}.charges ON (${SCHEMA}.charges.id = ${SCHEMA}.invoices.charge_id)
-                INNER JOIN ${SCHEMA}.subscriptions ON (${SCHEMA}.subscriptions.id = ${SCHEMA}.invoices.subscription_id)
-              WHERE
-                ${SCHEMA}.invoices.paid = TRUE AND
-                (${SCHEMA}.charges.refunded = FALSE OR ${SCHEMA}.charges.refunded IS NULL) AND
-                ${SCHEMA}.invoices_items.type = 'subscription'
-              ORDER BY
-                ${SCHEMA}.invoices.subscription_id
-            ) as paid_invoices_subquery
-            WHERE (invoice_amount - discount_amount) > 0`;
+const customerMovementSubquery = require('./SaaSMetricsSQL').customerMovementSubquery;
 
-const normalizedPaidInvoicesSubquery = `SELECT
-            *,
-            mrr - LAG(mrr) OVER(
-              PARTITION BY
-                subscription_id
-              ORDER BY
-                created
-            ) AS expansion_mrr,
+cube(`StripeSaaSMetrics`, {
+  title: `SaaS Metrics`,
+  sql: customerMovementSubquery,
 
-            (CASE WHEN LAG(mrr) OVER(
-              PARTITION BY
-                subscription_id
-              ORDER BY
-                created
-            ) IS NULL THEN 1 ELSE NULL END) as is_first_charge,
-            (CASE WHEN moment_last_invoice_id = invoice_id AND ended_at IS NOT NULL THEN 1 ELSE NULL END) as is_last_charge
-          FROM (${paidInvoicesSubquery}) as paid_invoices`;
+  joins: {
+    StripeCustomers: {
+      relationship: `belongsTo`,
+      sql: `${StripeSaaSMetrics}.stripe_customer_id = ${StripeCustomers}.id`
+    }
+  },
 
-const subscriptionStatus = `CASE WHEN is_first_charge = 1 THEN 1 WHEN is_last_charge = 1 THEN -1 ELSE 0 END`;
+  measures: {
+    mrr: {
+      title: `MRR`,
+      sql: `mrr`,
+      type: `sum`,
+      rollingWindow: {
+        trailing: `unbounded`,
+        offset: `end`
+      },
+      format: `currency`
+    },
 
-const allInvoicesSubquery = `SELECT
-            cc.*,
-            ${SCHEMA}.customers.description as customer_description,
-            ${SCHEMA}.customers.id as stripe_customer_id
-          FROM
-            (${normalizedPaidInvoicesSubquery}) AS cc,
-            ${SCHEMA}.subscriptions,
-            ${SCHEMA}.customers
-          WHERE
-            ${SCHEMA}.subscriptions.id = cc.subscription_id AND
-            ${SCHEMA}.customers.id = ${SCHEMA}.subscriptions.customer_id AND
-            (
-              ${SCHEMA}.subscriptions.ended_at > INTERVAL '28 day' +
-              COALESCE(${SCHEMA}.subscriptions.trial_end, ${SCHEMA}.subscriptions.start)
-            OR
-              ${SCHEMA}.subscriptions.ended_at IS NULL
-            )`;
+    churnedMrr: {
+      title: `Churned MRR`,
+      sql: `mrr`,
+      type: `sum`,
+      filters: [{
+        sql: `mrr_type = 'cancelled'`,
+      }]
+    },
 
-const mrrMovementSubquery = `WITH all_invoices AS (${allInvoicesSubquery})
+    newMrr: {
+      title: `New MRR`,
+      sql: `mrr`,
+      type: `sum`,
+      filters: [{
+        sql: `mrr_type = 'new'`,
+      }]
+    },
 
-          SELECT
-            created AS dd,
-            mrr,
-            currency,
-            plan_name,
-            customer_description,
-            stripe_customer_id,
-            'new' AS mrr_type,
-            1 subscription_status
-          FROM
-            all_invoices
-          WHERE
-            is_first_charge = 1
+    expansionMrr: {
+      title: `Expansion MRR`,
+      sql: `mrr`,
+      type: `sum`,
+      filters: [{
+        sql: `mrr_type = 'expansion'`,
+      }]
+    },
 
-          UNION ALL
-          SELECT
-            ended_at AS dd,
-            mrr * (-1) AS mrr,
-            currency,
-            plan_name,
-            customer_description,
-            stripe_customer_id,
-            'cancelled' AS mrr_type,
-            -1 subscription_status
-          FROM
-            all_invoices
-          WHERE
-            is_last_charge = 1
+    contractionMrr: {
+      title: `Contraction MRR`,
+      sql: `mrr`,
+      type: `sum`,
+      filters: [{
+        sql: `mrr_type = 'contraction'`,
+      }]
+    },
 
-          UNION ALL
-          SELECT
-            created AS dd,
-            expansion_mrr AS mrr,
-            currency,
-            plan_name,
-            customer_description,
-            stripe_customer_id,
-            (CASE WHEN expansion_mrr > 0 THEN 'expansion'
-                  WHEN expansion_mrr < 0 THEN 'contraction'
-            END) AS mrr_type,
-            0 subscription_status
-          FROM
-            all_invoices
-          WHERE
-            expansion_mrr IS NOT NULL AND
-            expansion_mrr <> 0`;
+    mrrMovement: {
+      title: `MRR Movement`,
+      sql: `mrr`,
+      type: `sum`,
+      format: `currency`
+    },
 
-const customerMovementSubquery = `SELECT 
-  *, 
-  SUM(subscription_status) OVER (PARTITION BY customer_description ORDER BY dd) as moment_active_subscriptions FROM 
-(
-  ${mrrMovementSubquery}
-) mrr_movement`
+    newSubscriptions: {
+      shown: false,
+      sql: `mrr`,
+      type: `count`,
+      filters: [{
+        sql: `mrr_type = 'new'`,
+      }]
+    },
 
-addExport({customerMovementSubquery: customerMovementSubquery});
+    averageSellingPrice: {
+      sql: `${newMrr} / NULLIF(${newSubscriptions}, 0)`,
+      type: `number`
+    },
+
+    churnedMovement30Days: {
+      shown: false,
+      sql: `mrr`,
+      filters: [{
+        sql: `mrr_type in ('cancelled', 'contraction')`,
+      }],
+      type: `sum`,
+      rollingWindow: {
+        trailing: `1 month`,
+        offset: `end`
+      },
+    },
+
+    mrr30daysAgo: {
+      shown: false,
+      sql: `mrr`,
+      type: `sum`,
+      rollingWindow: {
+        trailing: `unbounded`,
+        leading: `-1 month`,
+        offset: `end`
+      }
+    },
+
+    mrrChurnRate: {
+      title: `MRR Churn Rate`,
+      sql: `-100 * (${churnedMovement30Days}) / NULLIF(${mrr30daysAgo}, 0)`,
+      type: `number`,
+      format: `percent`,
+    },
+
+    activeCustomers: {
+      sql: `CASE
+                WHEN ${TABLE}.subscription_status = 1 AND ${TABLE}.moment_active_subscriptions = 1 THEN 1
+                WHEN ${TABLE}.subscription_status = -1 AND ${TABLE}.moment_active_subscriptions = 0 THEN -1
+                ELSE 0
+              END`,
+      type: `sum`,
+      rollingWindow: {
+        trailing: `unbounded`,
+        offset: `end`
+      }
+    },
+
+    arpa: {
+      title: `ARPA`,
+      sql: `${mrr} / NULLIF(${activeCustomers}, 0)`,
+      type: `number`,
+      format: `currency`,
+    },
+
+    ltv: {
+      title: `LTV`,
+      sql: `${arpa} / NULLIF(${mrrChurnRate} / 100.0, 0)`,
+      type: `number`,
+      format: `currency`
+    }
+  },
+
+  dimensions: {
+    plan: {
+      sql: `plan_name`,
+      type: `string`
+    },
+
+    // customer: {
+    //   sql: `customer_description`,
+    //   type: `string`
+    // },
+
+    movementType: {
+      sql: `mrr_type`,
+      type: `string`
+    },
+
+    time: {
+      sql: `dd`,
+      type: `time`
+    },
+
+    id: {
+      sql: `${CUBE}.dd || '-' || ${CUBE}.mrr_type || '-' || ${CUBE}.stripe_customer_id`,
+      type: `string`,
+      primaryKey: true
+    }
+  }
+});
